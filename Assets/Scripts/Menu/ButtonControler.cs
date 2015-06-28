c6184a23d5f0c3f3ce10adaf20498620442660c4
Kittus <markasenjo@hotmail.com>
2015-06-28 20:27:32 +0200

    Starting menus

---
+++ Assets/Scripts/Menu/ButtonControler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonControler : MonoBehaviour {
+	public Sprite modeBackground;
+	public GameObject playButton;
+	public Image background;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PlayButton(){
+		playButton.SetActive (false);
+		background.sprite = modeBackground;
+	}
+}
