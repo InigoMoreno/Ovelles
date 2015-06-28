16bcb384d33a14fa34e896ae507dca9825191914
Kittus <markasenjo@hotmail.com>
2015-06-28 21:07:30 +0200

    Main Menu Finished

--- Assets/Scripts/Menu/ButtonControler.cs
+++ Assets/Scripts/Menu/ButtonControler.cs
@@ -4,12 +4,16 @@ using UnityEngine.UI;
 
 public class ButtonControler : MonoBehaviour {
 	public Sprite modeBackground;
+	public Sprite mainBackground;
 	public GameObject playButton;
 	public Image background;
+	public GameObject modeButtons;
 
 	// Use this for initialization
 	void Start () {
-	
+		modeButtons.SetActive (false);
+		playButton.SetActive (true);
+		background.sprite = mainBackground;
 	}
 	
 	// Update is called once per frame
@@ -20,5 +24,6 @@ public class ButtonControler : MonoBehaviour {
 	public void PlayButton(){
 		playButton.SetActive (false);
 		background.sprite = modeBackground;
+		modeButtons.SetActive (true);
 	}
 }
