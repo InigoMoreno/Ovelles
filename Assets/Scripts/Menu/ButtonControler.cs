48f822d647f1f1a235d26706e69f52babc074d9a
Kittus <markasenjo@hotmail.com>
2015-06-29 19:00:08 +0200

    Menus acabats amb return button

--- Assets/Scripts/Menu/ButtonControler.cs
+++ Assets/Scripts/Menu/ButtonControler.cs
@@ -4,50 +4,20 @@ using UnityEngine.UI;
 
 public class ButtonControler : MonoBehaviour {
 
-	void Awake(){
-		DontDestroyOnLoad (gameObject);
-	}
-
 	// Intro
 	public void PlayButton(){
 		Application.LoadLevel (1);
-		StartCoroutine (WaitPlay (1f));
-	}
-	IEnumerator WaitPlay (float seconds){
-		yield return new WaitForSeconds(seconds);
-		
-		Button button = GameObject.Find("Solo").GetComponent<Button> ();
-		button.onClick.AddListener (delegate {
-			Campanya ();
-		});
 	}
 
 	//Menu Principal
 	public void Campanya(){
 		Application.LoadLevel (2);
-		StartCoroutine (WaitCampanya (1f));		
-	}
-	IEnumerator WaitCampanya (float seconds){
-		yield return new WaitForSeconds(seconds);
 
-		Button button = GameObject.Find("Jungle").GetComponent<Button> ();
-		button.onClick.AddListener (delegate {
-			Jungla ();
-		});
 	}
 
 	//Seleccio de Mapes
 	public void Jungla(){
-		Application.LoadLevel (3);
-		StartCoroutine (WaitJungla (1f));		
-	}
-	IEnumerator WaitJungla (float seconds){
-		yield return new WaitForSeconds(seconds);
-		
-		Button button = GameObject.Find("Level 1").GetComponent<Button> ();
-		button.onClick.AddListener (delegate {
-			Level1 ();
-		});
+		Application.LoadLevel (3);	
 	}
 
 	//Seleccio nivell
@@ -55,8 +25,10 @@ public class ButtonControler : MonoBehaviour {
 		Application.LoadLevel (4);
 	}
 
+	//Return
 	public void ReturnButton(){
-		Application.LoadLevel (Application.loadedLevel - 1);
+		int act = Application.loadedLevel;
+		Application.LoadLevel (act - 1);
 	}
 
 
