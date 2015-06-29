eb81b93a8f6876025d8c9d26b405ffee93693a1e
Kittus <markasenjo@hotmail.com>
2015-06-29 22:03:42 +0200

    music pawa

--- Assets/Scripts/Menu/ButtonControler.cs
+++ Assets/Scripts/Menu/ButtonControler.cs
@@ -3,30 +3,40 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class ButtonControler : MonoBehaviour {
+	private SoundManager soundmanager;
+
+	void Awake(){
+		soundmanager = GameObject.Find ("SoundManager").GetComponent<SoundManager> ();
+	}
 
 	// Intro
 	public void PlayButton(){
+		soundmanager.playeffect ();
 		Application.LoadLevel (1);
 	}
 
 	//Menu Principal
 	public void Campanya(){
+		soundmanager.playeffect ();
 		Application.LoadLevel (2);
 
 	}
 
 	//Seleccio de Mapes
 	public void Jungla(){
+		soundmanager.playeffect ();
 		Application.LoadLevel (3);	
 	}
 
 	//Seleccio nivell
 	public void Level1(){
+		soundmanager.playeffect ();
 		Application.LoadLevel (4);
 	}
 
 	//Return
 	public void ReturnButton(){
+		soundmanager.playeffect ();
 		int act = Application.loadedLevel;
 		Application.LoadLevel (act - 1);
 	}
---
+++ Assets/Scripts/Menu/SoundManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+	public static SoundManager instance = null;
+	public AudioSource music;
+	public AudioSource effect;
+
+
+	// Use this for initialization
+	void Awake () {
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+
+		DontDestroyOnLoad (gameObject);
+		music.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Application.loadedLevelName == "Level1")
+			music.Stop ();
+		else if (!music.isPlaying)
+			music.Play (); 
+	}
+
+	public void playeffect (){
+		effect.Play ();
+	}
+}
